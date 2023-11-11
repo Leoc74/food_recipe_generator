acaa6e94d451614ac6bde6cf4a1ef85890769929
Vaibhav Rathore <vaibhavrathore@vl965-172-31-174-142.wireless.umass.edu>
2023-11-11 18:38:10 -0500

    condensed code into methods, using one main

--- main.py
+++ main.py
@@ -2,25 +2,81 @@ import requests as req
 from bs4 import BeautifulSoup as bs
 from string import ascii_lowercase
 
-# original recipe page url
-baseUrl = 'https://www.foodnetwork.com/recipes/recipes-a-z/'
-# strings of links to search
-letUrls = []            
+def main():
+        # original recipe page url
+        baseUrl = 'https://www.foodnetwork.com/recipes/recipes-a-z/'
 
-page = req.get(baseUrl + 'a')
-# print(page.content)
-soup = bs(page.content, "html.parser")
-results = soup.find_all("li", class_ = "m-PromoList__a-ListItem")
+        # strings of links to search
+        letUrls = popUrls(baseUrl)      
+        
+        # search all letter urls
+        for url in letUrls:
+                # tuple containing url and title
+                recTups = searchLet(url)
+                for r in recTups:
+                        print(searchRecipe(r[0]))
 
-# loop through each link on the letter's page
-for r in results:
-        # get the link and title section
-        str1 = str(r.find_all('a'))
-        str1 += ','
-        # separate the link and title
-        url = str1.split('\"')[1]
-        title = str1.split(">")[1][:-3]
 
+def popUrls(base):
+        """ 
+        take a base url, add a letter for each webpage for recipes that start with that letter
+        
+        parameters:
+                base (str): base url to find branch links for
+        returns:
+                listUrls (list): list of urls with same base link
+        """
+        listUrls = []
+        # populate list of urls to search
+        for let in ascii_lowercase:
+                if let == 'x':
+                        let = 'xyz'
+                if let == 'y' or let == 'z':
+                        break
+                listUrls.append(base + let)
+        return listUrls
+
+
+def searchLet(url):    
+        """
+        take a url, search for all link sections
+        
+        parameters:
+                url (str): url to search for links on
+        returns:
+                sections (list): list of tuples that contain url and title of recipe
+        """    
+        lim = 20
+        # get request for url 
+        page = req.get(url)
+        # get the content, parse using bs
+        pageCont = bs(page.content, "html.parser")
+        # list of all sections that are recipes, limits to lim links found
+        sections = pageCont.find_all("li", class_ = "m-PromoList__a-ListItem", limit = lim)
+        
+        # list of tuples containing the url and title
+        urlTitleList = []
+        for r in sections:
+                # get the link and title section
+                tempSect = str(r.find_all('a'))
+                tempSect += ','
+                # separate the link and title
+                url = tempSect.split('\"')[1]
+                title = tempSect.split(">")[1][:-3]
+                # add a tuple containing link and title to list
+                urlTitleList.append((url, title))
+        return urlTitleList
+
+
+def searchRecipe(url):
+        """
+        take a section, get the link and url, get list of ingredients
+        
+        parameters:
+                url (str): url of recipe
+        returns:
+                ingrList (list): list of ingredients for recipe
+        """
         # recipe page
         recPage = req.get('https:' + url)
         # content from recipe page
@@ -28,74 +84,17 @@ for r in results:
         # get all of the ingredients (in checkboxes [html section])
         temp = rPcont.find("div", class_ = "o-Ingredients__m-Body")
         if(temp is None):
-                continue
+                return
         recipeSearch = str(temp.find_all("span", class_="o-Ingredients__a-Ingredient--CheckboxLabel"))
         # filtering out html formatting, keep title
         splitRec = recipeSearch.split('<span class="o-Ingredients__a-Ingredient--CheckboxLabel">')
         splitRec[-1] += ","
-        temp2 = list(map(lambda e: 
-                         e[:-9] if ("\xa0" not in e) else e[:-13]
-                         , splitRec))[2:]
-        print(temp2)
+        # list of ingredients
+        ingrList = list(map(lambda e: 
+                        e[:-9] if ("\xa0" not in e) else e[:-13]
+                        , splitRec))[2:]
+        return ingrList
 
 
-
-        
-
-
-
-# use this for final version
-# results = soup.find_all("li", class_ = "m-PromoList__a-ListItem")
-"""
-string1 = str(results.find("a"))
-
-url = string1.split('\"')[1]
-title = string1.split(">")[1][:-4]
-
-
-page2 = req.get('https:' + url)
-
-soup2 = bs(page2.content, "html.parser")
-res2 = str(soup2.find("div", class_ = "o-Ingredients__m-Body").find_all("span", class_="o-Ingredients__a-Ingredient--CheckboxLabel"))
-split2 = res2.split('<span class="o-Ingredients__a-Ingredient--CheckboxLabel">')
-split2[-1] += ","
-temp2 = list(map(lambda e: e[:-9], split2))[2:]
-"""
-
-"""
-# populate array of urls to search
-for let in ascii_lowercase:
-        if let == 'x':
-                let = 'xyz'
-        if let == 'y' or let == 'z':
-                break
-        letUrls.append(baseUrl + let)
-"""
-"""
-letUrls += page
-c1 = 0
-for url in letUrls:
-        # page of all links to recipe starting with letter
-        letPage = req.get(url)
-        
-        # beautifulsoup object, parses html content from page
-        lpCont = bs(letPage.content, "html.parser")
-
-        # finds all recipe headers, a list
-        results = lpCont.find_all("li", {"class" : "m-PromoList__a-ListItem"})
-        # get the a section in header
-        aStr = str(results.find("a"))
-
-        # separate the url and title from section
-        url = aStr.split('\"')[1]
-        title = aStr.split(">")[1][:-4]
-        # access the individual recipe 
-        page2 = req.get('https:' + url)
-        soup2 = bs(page2.content, "html.parser")
-
-        res2 = str(soup2.find("div", class_ = "o-Ingredients__m-Body").find_all("span", class_="o-Ingredients__a-Ingredient--CheckboxLabel"))
-        split2 = res2.split('<span class="o-Ingredients__a-Ingredient--CheckboxLabel">')
-        split2[-1] += ","
-        temp2 = list(map(lambda e: e[:-9], split2))[2:]
-        print(temp2)
-        """
+if __name__ == "__main__":
+        main()
